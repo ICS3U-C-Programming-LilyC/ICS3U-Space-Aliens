49505ee9797cf2a8d64e5083aa97adf2a80efc0e
Lily Carroll <lily.carroll@stu.ocsb.ca>
2024-01-10 13:00:50 -0500

    Added constants file

---
+++ constants.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Lily Carroll
+# This file holds my constants.
+
+# Pybadge screen size is 160x128 and sprites are 16x16.
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
