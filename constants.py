1379475bb502c4cab4a2aaadb686a9844bf352a8
Lily Carroll <lily.carroll@stu.ocsb.ca>
2024-01-12 12:46:22 -0500

    Got CPT-07 working

--- constants.py
+++ constants.py
@@ -15,8 +15,12 @@ SPRITE_MOVEMENT_SPEED = 1
 # Creating a dictionary to keep track of when the buttons were last pressed, since we want to be able to shoot 60 missiles in 60 seconds.
 # This allows for the button to be pressed and released quicker to achieve this timing for 60 missiles in 60 seconds.
 button_state = {
-    "button_up" : "up",
-    "button_just_pressed" : "just_pressed",
-    "button_still_pressed" : "still_pressed",
-    "button_released" : "released"
+    "button_up": "up",
+    "button_just_pressed": "just_pressed",
+    "button_still_pressed": "still_pressed",
+    "button_released": "released",
 }
+
+# Creating a red pallet to display the text in red on the PyBadge.
+RED_PALLETTE =(b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
